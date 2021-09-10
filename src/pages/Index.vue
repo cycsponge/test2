187318c7074da93fd2fb43e78f3c2367ef3a6a47
cycsponge <lisa293341@gmail.com>
2021-09-10 17:15:03 +0800

    test

---
+++ src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+    <h1>海綿的測試</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex"
+};
+</script>
